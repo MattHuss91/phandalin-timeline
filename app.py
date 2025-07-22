9d0284ff71164e190a3491838beb877c3b789854
MattHuss91 <matthew.husselbury@gmail.com>
2025-07-22 16:53:42 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+import streamlit as st
+
+st.title("The Heroes of Phandalin")
+st.write("The box was just the start")
+search_name = st.text_input("Search character name:")
+
+st.title("Phandalin Campaign Timeline")
+
+# Connect to database
+conn = sqlite3.connect("C:/Users/matth/OneDrive/dnd/Data Project/dnd_campaign.db")
+cursor = conn.cursor()
+
+# Get event timeline
+cursor.execute("""
+    SELECT date_occurred, title 
+    FROM CampaignEvents 
+    ORDER BY world_day;
+""")
+events = cursor.fetchall()
+
+# Display events
+for title, date in events:
+    st.subheader(date)
+    st.write(title)
+
