f17a2bdbdf6f98412143db2ed69845c5567b4624
MattHuss91 <matthew.husselbury@gmail.com>
2025-07-23 14:19:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.set_page_config(
     page_icon="📜",        # <- Optional: gives a nice emoji icon
     layout="centered"
 )
+st.title("The Heroes of Phandalin")
+st.write("The box was just the start")
