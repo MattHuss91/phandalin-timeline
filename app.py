334396564a60c4d3c3a47be51ff2300f177c67c6
MattHuss91 <matthew.husselbury@gmail.com>
2025-07-23 14:02:58 +0100

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-import streamlit as st
-
-st.set_page_config(page_title="Home", page_icon="📜", layout="centered")
-
-st.title("The Heroes of Phandalin")
-st.write("The box was just the start")
-  
