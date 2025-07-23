f0ee1798a2ecbb9d01b02170fefc4181f3f22c42
MattHuss91 <matthew.husselbury@gmail.com>
2025-07-23 14:14:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
 import streamlit as st
 
-# Hide streamlit's default menu and footer
-hide_streamlit_style = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    </style>
-"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
 # Redirect to Home
 st.switch_page("pages/Home.py")
