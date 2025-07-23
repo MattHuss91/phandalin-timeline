f638fa6588e19fe71d341264b278f67df3ec8ceb
MattHuss91 <matthew.husselbury@gmail.com>
2025-07-23 13:54:27 +0100

    set page title to home

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sqlite3
 
 import streamlit as st
 
-st.set_page_config(page_title="Home", layout="centered")
+st.set_page_config(page_title="Home", page_icon="📜", layout="centered")
 
 st.title("The Heroes of Phandalin")
 st.write("The box was just the start")
