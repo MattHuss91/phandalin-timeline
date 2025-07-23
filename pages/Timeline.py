be3cf33939f8159463713e26f083b4690c5aa4ab
MattHuss91 <matthew.husselbury@gmail.com>
2025-07-23 15:49:43 +0100

    Update Timeline.py

--- pages/Timeline.py
+++ pages/Timeline.py
@@ -6,28 +6,30 @@ st.set_page_config(page_title="Timeline", layout="centered")
 # Custom Fantasy Styling
 st.markdown("""
     <style>
-    @import url('https://fonts.googleapis.com/css2?family=Cinzel&family=Lora&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Uncial+Antiqua&family=Crimson+Pro&display=swap');
 
-    html, body, [class*="css"] {
-        font-family: 'Lora', serif !important;
-        color: #000000 !important;
-        background-image: url('https://i.imgur.com/pnRziEo.jpeg');
+    html, body, [class*="css"]  {
+        background-image: url("https://i.imgur.com/v0Jdhpp.jpeg");
         background-size: cover;
         background-attachment: fixed;
-        background-repeat: no-repeat;
         background-position: center;
+        color: #000000;
+        font-family: 'Crimson Pro', serif;
     }
 
-    .stContainer {
-        background-color: rgba(255, 255, 255, 0.9) !important;
+    h1, h2, h3, h4 {
+        font-family: 'Uncial Antiqua', cursive;
+        color: #000000;
+    }
+
+    .stMarkdown, .stText, .stSubheader, .stHeader, .stTitle {
+        color: #000000 !important;
+    }
+
+    .stApp {
+        background-color: rgba(255, 255, 255, 0.85);
         padding: 1rem;
         border-radius: 10px;
-    }
-
-    h1, h2, h3, h4, h5, h6 {
-        font-family: 'Cinzel', serif !important;
-        letter-spacing: 0.5px;
-        text-transform: uppercase;
     }
     </style>
 """, unsafe_allow_html=True)
