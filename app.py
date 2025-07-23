022fa6d220db670402d3bf296e9d2ea6caee8dab
MattHuss91 <matthew.husselbury@gmail.com>
2025-07-23 13:51:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import sqlite3
 
 import streamlit as st
 
+st.set_page_config(page_title="Home", layout="centered")
+
 st.title("The Heroes of Phandalin")
 st.write("The box was just the start")
   
