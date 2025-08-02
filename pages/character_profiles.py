15d701ed1a872b049631eda0a0d3153eb1b33bc1
Matt Husselbury <matthew.husselbury@gmail.com>
2025-08-02 20:10:24 +0100

    Update character_profiles.py

--- pages/character_profiles.py
+++ pages/character_profiles.py
@@ -6,23 +6,26 @@ import pandas as pd
 st.set_page_config(page_title="Character Profiles", layout="centered")
 
 # Styling
-st.markdown("""
+sst.markdown("""
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Cinzel&family=Lora&display=swap');
-    html, body, [class*="css"] {
-        font-family: 'Lora', serif !important;
-        color: #000000 !important;
+
+    .stApp {
         background-image: url('https://i.imgur.com/v0Jdhpp.jpeg');
         background-size: cover;
         background-attachment: fixed;
         background-repeat: no-repeat;
         background-position: center;
+        font-family: 'Lora', serif !important;
+        color: #000000 !important;
     }
+
     .stContainer {
         background-color: rgba(255, 255, 255, 0.9) !important;
         padding: 1rem;
         border-radius: 10px;
     }
+
     h1, h2, h3 {
         font-family: 'Cinzel', serif !important;
         text-transform: uppercase;
