d7088b98eb9cdc8078c654465c92cfb65ceffcd6
Matt Husselbury <matthew.husselbury@gmail.com>
2025-08-02 20:10:34 +0100

    Update home.py

--- home.py
+++ home.py
@@ -10,14 +10,14 @@ st.markdown("""
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Cinzel&family=Lora&display=swap');
 
-    html, body, [class*="css"] {
-        font-family: 'Lora', serif !important;
-        color: #000000 !important;
+    .stApp {
         background-image: url('https://i.imgur.com/v0Jdhpp.jpeg');
         background-size: cover;
         background-attachment: fixed;
         background-repeat: no-repeat;
         background-position: center;
+        font-family: 'Lora', serif !important;
+        color: #000000 !important;
     }
 
     .stContainer {
@@ -26,13 +26,13 @@ st.markdown("""
         border-radius: 10px;
     }
 
-    h1, h2, h3, h4, h5, h6 {
+    h1, h2, h3 {
         font-family: 'Cinzel', serif !important;
-        letter-spacing: 0.5px;
         text-transform: uppercase;
     }
     </style>
 """, unsafe_allow_html=True)
+
 
 st.title("The Heroes of Phandalin")
 st.write("The box was just the start")
