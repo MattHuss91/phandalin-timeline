3dc7d5227d857785f5beaddabdaea879c821d1a0
Matt Husselbury <matthew.husselbury@gmail.com>
2025-08-02 20:11:25 +0100

    Update character_profiles.py

--- pages/character_profiles.py
+++ pages/character_profiles.py
@@ -6,7 +6,7 @@ import pandas as pd
 st.set_page_config(page_title="Character Profiles", layout="centered")
 
 # Styling
-sst.markdown("""
+st.markdown("""
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Cinzel&family=Lora&display=swap');
 
