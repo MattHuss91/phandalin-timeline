060b27137214462151f6705b85f1e3eca8755174
Matt Husselbury <matthew.husselbury@gmail.com>
2025-08-03 12:17:04 +0100

    Update character_profiles.py

--- pages/character_profiles.py
+++ pages/character_profiles.py
@@ -127,6 +127,7 @@ st.write("### Bio")
 st.write(character_row["bio"])
 
 # --- Editable Bio Form ---
+with st.expander ("Edit Bio")
 with st.form("edit_bio_form"):
     new_bio = st.text_area("Edit Bio", character_row["bio"], height=200)
     submitted = st.form_submit_button("Save Changes")
