c99c67173a92b76ddf57195c8f7e30c836bd3947
Matt Husselbury <matthew.husselbury@gmail.com>
2025-08-05 19:13:56 +0100

    Update home.py

--- home.py
+++ home.py
@@ -35,7 +35,7 @@ st.markdown("""
         </style>
 """, unsafe_allow_html=True)
 
-st.image("https://i.imgur.com/MydOnTa.png")
+st.image("https://i.imgur.com/WEGvkz8.png")
 st.title("Loreweave")
 st.write("Heroes of Phandalin")
 
