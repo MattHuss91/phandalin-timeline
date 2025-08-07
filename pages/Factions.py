021ec4183f1ef8cda6bf80ed966e8f8cc6f608c3
Matt Husselbury <matthew.husselbury@gmail.com>
2025-08-07 14:25:24 +0100

    Update Factions.py

--- pages/Factions.py
+++ pages/Factions.py
@@ -65,9 +65,7 @@ except Exception as e:
 faction_df = pd.read_sql_query("SELECT faction_id, name, alignment, goals FROM factions ORDER BY name", conn)#
 
 # --- Faction dropdown ---
-faction_name = faction_df["name"].tolist()
-
-
+faction_name = sorted(faction_df["name"].tolist())
 selected_faction = st.selectbox("Choose a Faction", faction_name)
 faction_row = faction_df[faction_df["name"] == selected_faction].iloc[0]
 faction_id = int(faction_row["faction_id"])
@@ -101,6 +99,7 @@ else:
 
 st.markdown("---")
 st.caption("Loreweave")
+
 
 
 
