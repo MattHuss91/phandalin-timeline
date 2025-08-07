3e43b9248454b919460e7eb0105d5cf551de6d37
Matt Husselbury <matthew.husselbury@gmail.com>
2025-08-07 16:52:05 +0100

    Update home.py

--- home.py
+++ home.py
@@ -33,12 +33,64 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+#date config
+
+from datetime import date
+
+def get_current_fantasy_date():
+    # --- Config ---
+    fantasy_months = [
+        "Verdanir", "Emberfall", "Duskwatch", "Glimmerwane", "Brightreach",
+        "Stormrest", "Hollowshade", "Deepmoor", "Frostmere", "Starwake"
+    ]
+    weekdays = ["Sunsday", "Moonday", "Wyrmday", "Gloomday", "Thornsday", "Brightsday"]
+    days_per_month = 36
+    days_per_year = 360
+
+    # --- Date Calculations ---
+    anchor = date(2025, 1, 1)  # Real-world Day 1
+    today = date.today()
+    days_since_anchor = (today - anchor).days
+    world_day_number = days_since_anchor % days_per_year
+
+    # --- Fantasy Date ---
+    month_index = world_day_number // days_per_month
+    day_of_month = (world_day_number % days_per_month) + 1
+    weekday = weekdays[days_since_anchor % 6]
+
+    # --- Return as dict ---
+    return {
+        "weekday": weekday,
+        "day": day_of_month,
+        "month": fantasy_months[month_index],
+        "irl": today.strftime("%A %d %B")
+    }
+
 # logo
 st.markdown("""
     <div style='text-align: center; margin-top: -20px;'>
         <img src='https://i.imgur.com/WEGvkz8.png' style='width: 200px; margin-bottom: -10px;' />
         <h1 style='margin-top: 0; font-family: "Cinzel", serif;'>Heroes of Phandalin</h1>
     </div>
+""", unsafe_allow_html=True)
+
+# --- Get fantasy date ---
+fantasy_date = get_current_fantasy_date()
+
+# --- Display it ---
+st.markdown(f"""
+<div style='
+    background-color: rgba(255, 255, 255, 0.8);
+    padding: 0.75rem 1rem;
+    border-radius: 10px;
+    font-family: "Lora", serif;
+    text-align: center;
+    margin-bottom: 1rem;
+'>
+    📅 <strong>Today in the world:</strong><br>
+    <em>{fantasy_date['weekday']}, {fantasy_date['day']} {fantasy_date['month']}</em><br>
+    <small>(IRL: {fantasy_date['irl']})</small>
+</div>
 """, unsafe_allow_html=True)
 
 # User list
