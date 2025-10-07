61bde8594a64908ece13a75d6f7791a82b83d15a
Matt Husselbury <matthew.husselbury@gmail.com>
2025-10-07 09:33:47 +0100

    Update Factions.py

--- pages/Factions.py
+++ pages/Factions.py
@@ -13,7 +13,7 @@ st.markdown("""
     @import url('https://fonts.googleapis.com/css2?family=Cinzel&family=Lora&display=swap');
 
     .stApp {
-        background-image: url('https://i.imgur.com/v0Jdhpp.jpeg');
+        background-image: url('https://img.freepik.com/free-photo/wooden-floor-background_53876-88628.jpg');
         background-size: cover;
         background-attachment: fixed;
         background-repeat: no-repeat;
@@ -42,7 +42,7 @@ st.markdown("""
 # --- Title ---
 st.markdown("""
     <div style='text-align: center; margin-top: -20px;'>
-        <img src='https://i.imgur.com/WEGvkz8.png' style='width: 200px; margin-bottom: -10px;' />
+        <img src='https://i.postimg.cc/PNGx1Rqx/Copy-of-Loreweave.png' style='width: 200px; margin-bottom: -10px;' />
         <h1 style='margin-top: 0; font-family: "Cinzel", serif;'>Factions</h1>
     </div>
 """, unsafe_allow_html=True)
@@ -99,6 +99,7 @@ else:
 
 st.markdown("---")
 st.caption("Loreweave")
+
 
 
 
